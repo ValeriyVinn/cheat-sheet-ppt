1b70136f36dd4107b23da6b0b93ae48779d3e0ba
ValeriyVinn <115177117+ValeriyVinn@users.noreply.github.com>
2024-03-18 21:50:15 +0000

    Deploying to gh-pages from @ ValeriyVinn/cheat-sheet-ppt@1b1ec36b2786ee5ec4e3a26e981b40c2a9b73cc9 🚀

---
+++ index.9535d789.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-open-modal]"),t=document.querySelector("[data-close-modal]"),c=document.querySelector("[data-backdrop]");function d(){c.classList.toggle("is-hidden")}e.addEventListener("click",d),t.addEventListener("click",d),c.addEventListener("click",(function(){}));
+//# sourceMappingURL=index.9535d789.js.map
